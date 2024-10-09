f9b79db6135fe47e600786c04a7881561da22018
ThaiBaoNguyen <nthaibao1712@gmail.com>
2024-10-10 00:07:22 +0700

    Update 1

--- Models/Brand.cs
+++ Models/Brand.cs
@@ -10,6 +10,4 @@ public partial class Brand
     public string? Name { get; set; }
 
     public string? Description { get; set; }
-
-    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
 }
